1ac7101b7cc44488d5424f007830b194528d49fa
Michael <michaelbrandmeyer99@gmail.com>
2025-07-02 16:34:02 -0700

    added routes for , creatin folders, uploading files, deleting files

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,6 +2,8 @@ const { Router } = require("express");
 const indexController = require("../controllers/indexController");
 const indexRouter = Router();
 const { isAuth } = require("../controllers/authMiddleware");
+const multer  = require('multer')
+const upload = multer({ dest: './public/data/uploads/' })
 
 
 indexRouter.get("/", indexController.showHomePage);
@@ -10,6 +12,13 @@ indexRouter.post("/sign-up", indexController.SignUpPost);
 indexRouter.post("/log-in", indexController.LogInPost);
 indexRouter.get("/log-out", indexController.LogOutGet);
 
+indexRouter.get("/create-folder", indexController.CreateFolderGet);
+indexRouter.post("/create-folder", indexController.CreateFolderPost);
+
+indexRouter.post("/stats", upload.single('uploaded_file'), indexController.uploadPost);
+
+indexRouter.post("/delete-file/:fileId", isAuth, indexController.deleteFile);
+indexRouter.post("/delete-folder/:folderId", isAuth, indexController.deleteFolder);
 
 
 module.exports = indexRouter;
