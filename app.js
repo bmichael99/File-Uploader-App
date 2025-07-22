39589a550567e9f674e14868a065d0fc290cb122
Michael <michaelbrandmeyer99@gmail.com>
2025-07-21 20:10:26 -0700

    trust proxy for railway

--- app.js
+++ app.js
@@ -69,6 +69,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
+app.set('trust proxy', true);
 
 app.use((req, res, next) => {
   res.locals.success = req.flash('success');
