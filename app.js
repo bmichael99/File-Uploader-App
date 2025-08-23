4bc2f4c8d00efba79777cc92efe5d43e2c447551
Michael <michaelbrandmeyer99@gmail.com>
2025-08-23 12:06:48 -0700

    railway port

--- app.js
+++ app.js
@@ -157,7 +157,7 @@ app.use((err, req, res, next) => {
  */
 
 //starts the server and listens on port 3000
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const server = app.listen(PORT, () => {
   console.log(`My Express app - listening on port ${PORT}!`);
 });
